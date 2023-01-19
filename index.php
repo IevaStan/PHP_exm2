d7db750d38444f2e85d36a8d6c7561737b7e7fd3
IevaStan <101921345+IevaStan@users.noreply.github.com>
2023-01-19 19:43:17 +0200

    first task completed

--- index.php
+++ index.php
@@ -4,27 +4,17 @@ include_once "Employee.php";
 include_once "Intern.php";
 include_once "Manager.php";
 
+$intern = new Intern("Jonas Jonaitis", 3450);
+echo $intern. PHP_EOL;
+echo $intern->calculateSalary() . PHP_EOL;
+echo Intern::getTypeDescription() . PHP_EOL;
 
+$manager = new Manager("Petra Petraitienė", 3600, 60);
+echo $manager. PHP_EOL;
+echo $manager->calculateSalary() . PHP_EOL;
+echo Manager::getTypeDescription() . PHP_EOL;
 
-// - Getter ir Setter turi būti sukurtas experience properčiui. [0.25 balo]
-// - Getter methodas turi būti sukurtas id properčiui [0.25 balo]
-// - Metodas public calculateSalary() turi grąžinti salary reikšmę [0.5 balo]
-// - Magic methodas toString turi grąžinti string: "Darbuotojas {id}: {name} - {salary} EUR" [0.25 balo]
-// - Magic methodas clone turi pakeisti id verte į 0 [0.25 balo]
-// - static methodas getTypeDescription() gražina string "Darbuotojo tipas {constantos verte}" [0.5 balo]
-
-
-// Intern.php turi extendinti Employee klasę ir viskas: [0.25 balo]
-
-// Manager.php turi extendinti Employee klasę: [1 balas]
-// - constantos EMPLOYEE_TYPE reksme turi būti 2.
-// - papildomas propertis owertimeHours
-// - construktorius turi priimti dar papildomai owertimeHours, nepamirškite extendinti ir parent construktoriaus;
-// - Perrašykite calculateSalary() funkciją. Suskaičiuokite atlyginima: salary + (salary / 40 *  owertimeHours) * 2;
-
-
-// Sukurkite Intern ir Manager objektus.    [0.5 balo]
-// Išspausdinkite tiesiogiai abu objektus
-// Išveskite jų atlyginimus.
-// Išveskite getTypeDescription;
-// Paklonuokite intern sukurtą objektą
+$intern2 = clone $intern;
+echo $intern2. PHP_EOL;
+echo $intern2->calculateSalary() . PHP_EOL;
+echo Intern::getTypeDescription() . PHP_EOL;
